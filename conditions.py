c7c8d12579512ce1994cf02ff608cca7267dd139
Aron Bergman <bathingrad@gmail.com>
2023-01-27 10:07:46 +0100

    Create skeleton for conditions
    Useful for #4

---
+++ conditions.py
@@ -0,0 +1,46 @@
+import declarations as d
+
+def cond_0():
+	return True
+
+def cond_1():
+	return True
+
+def cond_2():
+	return True
+
+def cond_3():
+	return True
+
+def cond_4():
+	return True
+
+def cond_5():
+	return True
+
+def cond_6():
+	return True
+
+def cond_7():
+	return True
+
+def cond_8():
+	return True
+
+def cond_9():
+	return True
+
+def cond_10():
+	return True
+
+def cond_11():
+	return True
+
+def cond_12():
+	return True
+
+def cond_13():
+	return True
+
+def cond_14():
+	return True
