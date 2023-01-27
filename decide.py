5551a767d822a63bf6b9a544f7775df2e2b6711c
William Berg <wm.bg@outlook.com>
2023-01-27 10:10:49 +0100

    template changes #1

--- decide.py
+++ decide.py
@@ -7,7 +7,7 @@ import numpy as np
 def decide():
 	CMV = set_CMV()
 	PUM = set_PUM(CMV)
-	FUV = set_FUV(PUM, d.PUV_MATRIX)
+	FUV = set_FUV(PUM)
 
 	if np.all(FUV):
 		launch = True
@@ -20,7 +20,7 @@ def decide():
 # CMV is a boolean vector whose elements have a one-to-one correspondence with the LICs
 # If the radar tracking data satisfy a certain LIC, then the corresponding element of the CMV is set to true.
 def set_CMV():
-	CMV = np.full(shape=15, fill_value=False)
+	CMV = np.full(shape=d.DIMENSION, fill_value=False)
 
 	return CMV
 
@@ -28,16 +28,16 @@ def set_CMV():
 # Every element of the boolean PUM corresponds to an element of the LCM.
 # If the logical connection dictated by the LCM element gives the value “true”, the corresponding PUM element is set to true.
 def set_PUM(CMV):
-	PUM = np.full(shape=(15, 15), fill_value=False)
+	PUM = np.full(shape=(d.DIMENSION, d.DIMENSION), fill_value=False)
 
 	return PUM
 
 
 # FUV is a boolean vector which is the basis for deciding whether to launch
 # If all elements of the FUV are true, a launch should occur.
-def set_FUV(PUM, PUV_MATRIX):
+def set_FUV(PUM):
 	FUV = np.full(shape=d.DIMENSION, fill_value=False)
-	
+
 	return FUV
 
 
